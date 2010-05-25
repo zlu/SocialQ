083b155071aa4782b944c5c6f18e7ef049f56d62
Jason <jason@goecke.net>
2010-05-25 15:25:31 -0700

    Debug code to figure out why our hash back from Mongo is mangled.

--- app.rb
+++ app.rb
@@ -38,10 +38,10 @@ def fetch_scenarios
   collection = db.collection('socialq_scenarios')  
   scenarios = []
   collection.find.each { |doc| scenarios << doc }
-  scenarios.delete('_id')
+  scenarios[0].delete('_id')
   @@log.info scenarios.inspect
   @@log.info '*'*10
-  scenarios
+  scenarios[0]
 end
 
 # Section or dealng wth Tropo WebAPI
