2377ac4224c284ea75618810438903dab4518af8
Jason <jason@goecke.net>
2010-05-25 15:21:40 -0700

    Debug code to figure out why our hash back from Mongo is mangled.

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ def fetch_scenarios
   collection = db.collection('socialq_scenarios')  
   scenarios = []
   collection.find.each { |doc| scenarios << doc }
-  scenarios.delete('_id')
+  scenarios.delete!('_id')
   scenarios
 end
 
