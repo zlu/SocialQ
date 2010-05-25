6aa1b6f5d453ae3139c68b2217397a566de237ea
Jason <jason@goecke.net>
2010-05-24 17:35:32 -0700

    Added a simple HTML GUI to process queues

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 $: << File.expand_path(File.dirname(__FILE__))
-%w(rubygems logger restclient).each { |lib| require lib }
+%w(rubygems logger restclient haml uri).each { |lib| require lib }
 
 # Load the configuration
 APP_CONFIG = YAML.load(File.open('config/application.yml'))
+SCENARIOS = { 
+    'basic' => [ {"agents"=>[], "users"=>[{"klout"=>{"result"=>"Not found"}, "guid"=>"2e7e9155-865d-4fa2-800e-441be3013bf7", "queue_name"=>"1273697421", "tweet_watchword"=>nil, "time"=>"Wed May 12 13:50:21 -0700 2010", "phone_number"=>"4074181800", "channel"=>"phone", "queue_weight"=>10, "social_influence_rank"=>0, "twitter_profile"=>{"profile_background_tile"=>true, "profile_sidebar_fill_color"=>"DDEEF6", "profile_sidebar_border_color"=>"C0DEED", "name"=>"LenosSquirrel", "created_at"=>"Thu Mar 11 01:12:27 +0000 2010", "profile_image_url"=>"http://a1.twimg.com/profile_images/745430850/squirrel_normal.jpg", "location"=>"Malibu", "profile_link_color"=>"0084B4", "contributors_enabled"=>false, "url"=>nil, "favourites_count"=>0, "id"=>121916400, "utc_offset"=>-28800, "lang"=>"en", "followers_count"=>2, "protected"=>false, "profile_text_color"=>"333333", "description"=>"Glad Leno stays out later these days.", "notifications"=>false, "geo_enabled"=>false, "profile_background_color"=>"C0DEED", "time_zone"=>"Pacific Time (US & Canada)", "verified"=>false, "profile_background_image_url"=>"http://a1.twimg.com/profile_background_images/82164118/acorn_large.jpg", "status"=>{"coordinates"=>nil, "favorited"=>false, "truncated"=>false, "created_at"=>"Wed May 12 20:44:11 +0000 2010", "contributors"=>nil, "text"=>"I am tired of being in queue! #fail", "id"=>13870917469, "geo"=>nil, "in_reply_to_user_id"=>nil, "source"=>"<a href=\"http://www.atebits.com/\" rel=\"nofollow\">Tweetie</a>", "place"=>nil, "in_reply_to_screen_name"=>nil, "in_reply_to_status_id"=>nil}, "statuses_count"=>51, "friends_count"=>5, "following"=>false, "screen_name"=>"LenosSquirrel"}}]},
+                 {"agents"=>[], "users"=>[{"klout"=>{"result"=>"Not found"}, "guid"=>"2e7e9155-865d-4fa2-800e-441be3013bf7", "queue_name"=>"1273697421", "tweet_watchword"=>nil, "time"=>"Wed May 12 13:50:21 -0700 2010", "phone_number"=>"4074181800", "channel"=>"phone", "queue_weight"=>10, "social_influence_rank"=>0, "twitter_profile"=>{"profile_background_tile"=>true, "profile_sidebar_fill_color"=>"DDEEF6", "profile_sidebar_border_color"=>"C0DEED", "name"=>"LenosSquirrel", "created_at"=>"Thu Mar 11 01:12:27 +0000 2010", "profile_image_url"=>"http://a1.twimg.com/profile_images/745430850/squirrel_normal.jpg", "location"=>"Malibu", "profile_link_color"=>"0084B4", "contributors_enabled"=>false, "url"=>nil, "favourites_count"=>0, "id"=>121916400, "utc_offset"=>-28800, "lang"=>"en", "followers_count"=>2, "protected"=>false, "profile_text_color"=>"333333", "description"=>"Glad Leno stays out later these days.", "notifications"=>false, "geo_enabled"=>false, "profile_background_color"=>"C0DEED", "time_zone"=>"Pacific Time (US & Canada)", "verified"=>false, "profile_background_image_url"=>"http://a1.twimg.com/profile_background_images/82164118/acorn_large.jpg", "status"=>{"coordinates"=>nil, "favorited"=>false, "truncated"=>false, "created_at"=>"Wed May 12 20:44:11 +0000 2010", "contributors"=>nil, "text"=>"I am tired of being in queue! #fail", "id"=>13870917469, "geo"=>nil, "in_reply_to_user_id"=>nil, "source"=>"<a href=\"http://www.atebits.com/\" rel=\"nofollow\">Tweetie</a>", "place"=>nil, "in_reply_to_screen_name"=>nil, "in_reply_to_status_id"=>nil}, "statuses_count"=>51, "friends_count"=>5, "following"=>false, "screen_name"=>"LenosSquirrel"}}, {"klout"=>{"twitter_screen_name"=>"aplusk", "twitter_id"=>"19058681", "score"=>{"kscore"=>83.94, "kclass_description"=>"You have built a personal brand around your identity. There is a good chance that you work in social media or marketing but you might even be famous in real life. Being a persona is not just about having a ton of followers, to make it to the top right corner you need to engage with your audience. Make no mistake about it though, when you talk people listen.", "slope"=>-0.08, "amplification_score"=>76.15, "network_score"=>86.27, "kscore_description"=>nil, "true_reach"=>1928961, "kclass"=>"persona"}}, "guid"=>"c6d6dae0-f5f0-4c27-a3c6-669fcafa8295", "queue_name"=>"1273697438", "tweet_watchword"=>nil, "time"=>"Wed May 12 13:50:38 -0700 2010", "phone_number"=>"jsgoecke", "channel"=>"phone", "queue_weight"=>177.88, "social_influence_rank"=>10, "twitter_profile"=>{"profile_background_tile"=>true, "profile_sidebar_fill_color"=>"DDFFCC", "profile_sidebar_border_color"=>"BDDCAD", "name"=>"ashton kutcher", "created_at"=>"Fri Jan 16 07:40:06 +0000 2009", "profile_image_url"=>"http://a1.twimg.com/profile_images/638714290/profile_pic_normal.jpg", "location"=>"Los Angeles, California", "profile_link_color"=>"8f000e", "favourites_count"=>73, "url"=>"http://www.facebook.com/Ashton", "contributors_enabled"=>false, "id"=>19058681, "utc_offset"=>-18000, "lang"=>"en", "protected"=>false, "followers_count"=>4882753, "profile_text_color"=>"333333", "description"=>"I make stuff, actually I make up stuff, stories mostly, collaborations of thoughts, dreams, and actions. Thats me.", "geo_enabled"=>false, "notifications"=>false, "verified"=>true, "time_zone"=>"Eastern Time (US & Canada)", "profile_background_color"=>"9AE4E8", "friends_count"=>552, "status"=>{"coordinates"=>nil, "favorited"=>false, "truncated"=>false, "created_at"=>"Wed May 12 03:01:27 +0000 2010", "contributors"=>nil, "text"=>"Watch the 1 girl in the back row that realizes she's witnessing a future superstar in the making AMAZING-&gt; http://bit.ly/b8Ie3M", "id"=>13827306741, "geo"=>nil, "in_reply_to_user_id"=>nil, "source"=>"<a href=\"http://www.brizzly.com\" rel=\"nofollow\">Brizzly</a>", "place"=>nil, "in_reply_to_screen_name"=>nil, "in_reply_to_status_id"=>nil}, "statuses_count"=>5381, "profile_background_image_url"=>"http://a3.twimg.com/profile_background_images/56586067/Picture_4.png", "following"=>false, "screen_name"=>"aplusk"}}]},
+                 {"agents"=>[], "users"=>[{"klout"=>{"result"=>"Not found"}, "guid"=>"2e7e9155-865d-4fa2-800e-441be3013bf7", "queue_name"=>"1273697421", "tweet_watchword"=>"fail", "time"=>"Wed May 12 13:50:21 -0700 2010", "phone_number"=>"4074181800", "channel"=>"phone", "queue_weight"=>15, "social_influence_rank"=>0, "twitter_profile"=>{"profile_background_tile"=>true, "profile_sidebar_fill_color"=>"DDEEF6", "profile_sidebar_border_color"=>"C0DEED", "name"=>"LenosSquirrel", "created_at"=>"Thu Mar 11 01:12:27 +0000 2010", "profile_image_url"=>"http://a1.twimg.com/profile_images/745430850/squirrel_normal.jpg", "location"=>"Malibu", "profile_link_color"=>"0084B4", "contributors_enabled"=>false, "url"=>nil, "favourites_count"=>0, "id"=>121916400, "utc_offset"=>-28800, "lang"=>"en", "followers_count"=>2, "protected"=>false, "profile_text_color"=>"333333", "description"=>"Glad Leno stays out later these days.", "notifications"=>false, "geo_enabled"=>false, "profile_background_color"=>"C0DEED", "time_zone"=>"Pacific Time (US & Canada)", "verified"=>false, "profile_background_image_url"=>"http://a1.twimg.com/profile_background_images/82164118/acorn_large.jpg", "status"=>{"coordinates"=>nil, "favorited"=>false, "truncated"=>false, "created_at"=>"Wed May 12 20:44:11 +0000 2010", "contributors"=>nil, "text"=>"I am tired of being in queue! #fail", "id"=>13870917469, "geo"=>nil, "in_reply_to_user_id"=>nil, "source"=>"<a href=\"http://www.atebits.com/\" rel=\"nofollow\">Tweetie</a>", "place"=>nil, "in_reply_to_screen_name"=>nil, "in_reply_to_status_id"=>nil}, "statuses_count"=>51, "friends_count"=>5, "following"=>false, "screen_name"=>"LenosSquirrel"}}, {"klout"=>{"twitter_screen_name"=>"aplusk", "twitter_id"=>"19058681", "score"=>{"kscore"=>83.94, "kclass_description"=>"You have built a personal brand around your identity. There is a good chance that you work in social media or marketing but you might even be famous in real life. Being a persona is not just about having a ton of followers, to make it to the top right corner you need to engage with your audience. Make no mistake about it though, when you talk people listen.", "slope"=>-0.08, "amplification_score"=>76.15, "network_score"=>86.27, "kscore_description"=>nil, "true_reach"=>1928961, "kclass"=>"persona"}}, "guid"=>"c6d6dae0-f5f0-4c27-a3c6-669fcafa8295", "queue_name"=>"1273697438", "tweet_watchword"=>nil, "time"=>"Wed May 12 13:50:38 -0700 2010", "phone_number"=>"jsgoecke", "channel"=>"phone", "queue_weight"=>177.88, "social_influence_rank"=>10, "twitter_profile"=>{"profile_background_tile"=>true, "profile_sidebar_fill_color"=>"DDFFCC", "profile_sidebar_border_color"=>"BDDCAD", "name"=>"ashton kutcher", "created_at"=>"Fri Jan 16 07:40:06 +0000 2009", "profile_image_url"=>"http://a1.twimg.com/profile_images/638714290/profile_pic_normal.jpg", "location"=>"Los Angeles, California", "profile_link_color"=>"8f000e", "favourites_count"=>73, "url"=>"http://www.facebook.com/Ashton", "contributors_enabled"=>false, "id"=>19058681, "utc_offset"=>-18000, "lang"=>"en", "protected"=>false, "followers_count"=>4882753, "profile_text_color"=>"333333", "description"=>"I make stuff, actually I make up stuff, stories mostly, collaborations of thoughts, dreams, and actions. Thats me.", "geo_enabled"=>false, "notifications"=>false, "verified"=>true, "time_zone"=>"Eastern Time (US & Canada)", "profile_background_color"=>"9AE4E8", "friends_count"=>552, "status"=>{"coordinates"=>nil, "favorited"=>false, "truncated"=>false, "created_at"=>"Wed May 12 03:01:27 +0000 2010", "contributors"=>nil, "text"=>"Watch the 1 girl in the back row that realizes she's witnessing a future superstar in the making AMAZING-&gt; http://bit.ly/b8Ie3M", "id"=>13827306741, "geo"=>nil, "in_reply_to_user_id"=>nil, "source"=>"<a href=\"http://www.brizzly.com\" rel=\"nofollow\">Brizzly</a>", "place"=>nil, "in_reply_to_screen_name"=>nil, "in_reply_to_status_id"=>nil}, "statuses_count"=>5381, "profile_background_image_url"=>"http://a3.twimg.com/profile_background_images/56586067/Picture_4.png", "following"=>false, "screen_name"=>"aplusk"}}]} ],
+                 
+     'high_influence' => [ { 'foo' => 'bar' } ]
+  }
 
 # Start the logger
 @log = Logger.new(STDOUT)
@@ -13,6 +20,8 @@ APP_CONFIG = YAML.load(File.open('config/application.yml'))
 
 set :sessions, true
 set :port, APP_CONFIG['sinatra']['port']
+set :views, File.dirname(__FILE__) + '/templates'
+set :haml, { :format => :html5 }
 
 def connect_to_rabbit(queue)
   bunny = Bunny.new(:user    => APP_CONFIG['rabbit_mq']['user'],
@@ -88,7 +97,7 @@ get '/messages' do
   messages.to_json
 end
 
-get '/scenario' do
+get '/dump' do
   dumpq = connect_to_rabbit('dumpq')
   
   messages = Array.new
@@ -112,15 +121,32 @@ post '/agent_ready' do
   agentq.publish(request.env["rack.input"].read)
 end
 
-post '/reset' do
+get '/reset' do
   resetq = connect_to_rabbit('resetq')
-  resetq.publish(request.env["rack.input"].read)
+  resetq.publish({ :action => 'reset' }.to_json)
+  haml :reset
 end
 
-get '/test' do
-  { :foo => 'bar' }.to_json
+get '/scenarios' do
+  @scenarios = SCENARIOS
+  haml :scenarios
 end
 
-get '/scenario_app' do
+get '/scenario/:scenario' do |scenario|
+  @scenario, session['scenario'] = scenario, scenario
+  @possible_scenarios = ''
+  SCENARIOS.each { |k, v| @possible_scenarios += k + ' ' }
+  @messages = SCENARIOS[scenario]
   
+  if @messages
+    haml :scenario
+  else
+    haml :unkown
+  end
+end
+
+get '/posts/:message_number' do |message_number|
+  socialq = connect_to_rabbit('dumpq')
+  socialq.publish(SCENARIOS[session['scenario']][message_number.to_i].to_json)
+  redirect "/scenario/#{session['scenario']}"
 end
